9fc701ac85566e39b510b9fab2ee3b81f3de08a2
meysam-mfard <mmohagheghi@gmail.com>
2019-08-21 10:48:12 +0200

    Init project

---
+++ src/test/java/mej/spring/webfluxrest/WebfluxRestApplicationTests.java
@@ -0,0 +1,16 @@
+package mej.spring.webfluxrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebfluxRestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
