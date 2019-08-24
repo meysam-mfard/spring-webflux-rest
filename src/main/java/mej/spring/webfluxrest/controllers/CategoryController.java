c09d97ad980ae856b3fef98884f56c823c38aa40
meysam-mfard <mmohagheghi@gmail.com>
2019-08-24 16:54:43 +0200

    Add category PUT and POST api and their tests.

--- src/main/java/mej/spring/webfluxrest/controllers/CategoryController.java
+++ src/main/java/mej/spring/webfluxrest/controllers/CategoryController.java
@@ -2,9 +2,9 @@ package mej.spring.webfluxrest.controllers;
 
 import mej.spring.webfluxrest.domain.Category;
 import mej.spring.webfluxrest.repositories.CategoryRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -25,5 +25,17 @@ public class CategoryController {
     @GetMapping("/api/v1/category/{id}")
     public Mono<Category> getCategory(@PathVariable String id) {
         return categoryRepository.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/api/v1/categories")
+    public Mono<Void> postCategory(@RequestBody Publisher<Category> categoryStream) {
+        return categoryRepository.saveAll(categoryStream).then();
+    }
+
+    @PutMapping("/api/v1/category/{id}")
+    public Mono<Category> putCategory(@PathVariable  String id, @RequestBody Category category) {
+        category.setId(id);
+        return categoryRepository.save(category);
     }
 }
--- src/test/java/mej/spring/webfluxrest/controllers/CategoryControllerTest.java
+++ src/test/java/mej/spring/webfluxrest/controllers/CategoryControllerTest.java
@@ -5,15 +5,17 @@ import mej.spring.webfluxrest.repositories.CategoryRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.BDDMockito;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import static org.mockito.ArgumentMatchers.any;
+
 public class CategoryControllerTest {
 
+    private static final String CATEGORY_ID = "dummyId";
     CategoryRepository categoryRepository;
     CategoryController categoryController;
     WebTestClient webTestClient;
@@ -38,12 +40,44 @@ public class CategoryControllerTest {
 
     @Test
     public void getCategory() {
-        BDDMockito.given(categoryRepository.findById("dummyId"))
+        BDDMockito.given(categoryRepository.findById(CATEGORY_ID))
                 .willReturn(Mono.just(new Category("Cat")));
 
-        webTestClient.get()
-                .uri("/api/v1/categories/dummyId")
+        webTestClient
+                .get()
+                .uri("/api/v1/category/" + CATEGORY_ID)
                 .exchange()
                 .expectBody(Category.class);
+    }
+
+    @Test
+    public void postCategory() {
+        BDDMockito.given(categoryRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(new Category("cat")));
+
+        Mono<Category> catToSave = Mono.just(new Category("some cat"));
+
+        webTestClient.post()
+                .uri("/api/v1/categories")
+                .body(catToSave, Category.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+    }
+
+
+    @Test
+    public void putCategory() {
+        BDDMockito.given(categoryRepository.save(any(Category.class)))
+                .willReturn(Mono.just(new Category()));
+
+        Mono<Category> catToUpdate = Mono.just(new Category("some cat"));
+
+        webTestClient.put()
+                .uri("/api/v1/category/" + CATEGORY_ID)
+                .body(catToUpdate, Category.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
     }
 }
